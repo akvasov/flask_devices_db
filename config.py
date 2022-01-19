0f9a97eeae1122105c0dadbb209bb81876bf4fcb
andrew.kvasoff@gmail.com <35040802+akvasov@users.noreply.github.com>
2022-01-19 17:47:32 -0500

    Moved flask config variable into dedicated file

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import os
 from dotenv import load_dotenv
 load_dotenv() #load environment variables from .env file
 
-
-db_host = os.environ.get('DB_HOST')
-db_name = os.environ.get('DB_NAME')
-db_password = os.environ.get('DB_PASSWORD')
-db_port = os.environ.get('DB_PORT')
-db_user = os.environ.get('DB_USERNAME')
-
-SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+class Config(object):
+    db_host = os.environ.get('DB_HOST')
+    db_name = os.environ.get('DB_NAME')
+    db_password = os.environ.get('DB_PASSWORD')
+    db_port = os.environ.get('DB_PORT')
+    db_user = os.environ.get('DB_USERNAME')
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    PROPAGATE_EXCEPTIONS = True
